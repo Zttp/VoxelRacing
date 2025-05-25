bb8e63edd1321ddcd17b7c5861e71b344c3d6849
Zttp <162319093+Zttp@users.noreply.github.com>
2025-05-25 23:08:53 +0300

    Update hud.gd

--- hud.gd
+++ hud.gd
@@ -1,17 +1,37 @@
 extends CanvasLayer
 
+@onready var total_bananas_label: Label = $TotalBananasLabel
+@onready var collected_bananas_label: Label = $CollectedBananasLabel
+
+var session_bananas: int = 0  # Бананы, собранные в текущей сессии
+
 func _ready():
-    update_mission_text("first_ride")
+    # Инициализация при старте
+    update_total_bananas()
+    update_collected_bananas()
+    
+    # Подключаем сигналы
+    GameState.bananas_changed.connect(update_total_bananas)
 
-func update_mission_text(mission_id: String):
-    var text = ""
-    match mission_id:
-        "first_ride":
-            text = "Миссия: Доедь до финиша!"
-        "banana_collector":
-            text = "Миссия: Собери 15 бананов"
-    $MissionLabel.text = text
+func update_total_bananas():
+    """Обновляет отображение общих сохранённых бананов"""
+    total_bananas_label.text = "Всего: %d🍌" % GameState.bananas
 
-func _on_finish_zone_body_entered(body):
-    if body.is_in_group("player"):
-        MissionManager.complete_mission("first_ride")
+func update_collected_bananas():
+    """Обновляет отображение бананов, собранных в уровне"""
+    collected_bananas_label.text = "Собрано: %d🍌" % session_bananas
+
+func add_bananas(amount: int):
+    """Добавляет временные бананы (для уровня)"""
+    session_bananas += amount
+    update_collected_bananas()
+
+func save_session_bananas():
+    """Переносит собранные бананы в GameState"""
+    GameState.add_bananas(session_bananas)
+    session_bananas = 0
+    update_collected_bananas()
+
+func _on_finish_zone_entered():
+    """Пример: сохраняем бананы при завершении уровня"""
+    save_session_bananas()
